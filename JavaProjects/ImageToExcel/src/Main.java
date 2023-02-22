6387a96d21aa06a4fb7c1eab1a615316d9fcfe51
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-02-22 17:06:59 +0500

    3

--- JavaProjects/ImageToExcel/src/Main.java
+++ JavaProjects/ImageToExcel/src/Main.java
@@ -1,8 +1,21 @@
-
-
+import java.io.FileOutputStream;
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!egeeg");
+
+        /*System.out.print("Ввод: ");
+
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+
+        System.out.println(line);*/
+
+        String file = "calendar.xls";
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            wb.write(out);
+        }
     }
 }
---
+++ JavaProjects/test2/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Ввод: ");
+
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+
+        System.out.println(line);
+    }
+}
