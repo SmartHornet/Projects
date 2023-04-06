8e8725497d277aa3e8b0f42c0306f9a47062e843
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-04-06 22:09:18 +0500

    06.04.2023 22:09

--- JavaProjects/taskManager/src/Task.java
+++ JavaProjects/taskManager/src/Task.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 
 public class Task extends TaskPrototype {
-    ArrayList<Task> subTasks;
-    Task parentTask;
+    private ArrayList<Task> subTasks;
+    private Task parentTask;
+    int status;
+
+    public static final int NEW = 0;
+    public static final int DONE = 1;
+    public static final int IN_PROGRESS = 2;
 
     Task(String name, String description){
         super(name, description);
+        status = NEW;
     }
 }
