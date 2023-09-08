8597586b3c9ac71fb2ebdc4f17b920feb2166293
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-09-08 19:27:02 +0500

    08.09.23_19:27

--- JavaProjects/Yandex practikum/1 Java Core 1 часть/Спринт 4/FindAlgorithms/src/Main.java
+++ JavaProjects/Yandex practikum/1 Java Core 1 часть/Спринт 4/FindAlgorithms/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int m[] = new int[]{40,3,48,76,72,96,43,25,46,72};
+        int m[] = new int[]{40,3,48,76,16,96,43,10,46,72};
 
 
        for (int i = 0; i < 10; i++){
@@ -12,5 +12,40 @@ public class Main {
            //System.out.print(Math.round(Math.random()*100)+",");
 
        }
+
+
+
+    }
+
+
+
+    public int[] quickSort(int[] array){
+        if(array.length < 1) return array;
+
+
+
+
+    }
+
+    private int[] quickSort(int left, int right, int[] array){
+        int pivot = array[(right-left)/2];
+        //int left = 0;
+        //int right = array.length - 1;
+        int temp;
+
+        while (left < right) {
+            while (array[left] < pivot) left++;
+            while (array[right] > pivot) right--;
+
+            temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+        }
+    }
+
+
+    private void partition(int[] array) {
+
+
     }
 }
