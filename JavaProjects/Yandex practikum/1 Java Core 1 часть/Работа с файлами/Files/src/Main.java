332f376cd0d11d696610bdc84343803b83f15c33
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-10-06 17:53:40 +0500

    06.10.23_17:53

--- JavaProjects/Yandex practikum/1 Java Core 1 часть/Работа с файлами/Files/src/Main.java
+++ JavaProjects/Yandex practikum/1 Java Core 1 часть/Работа с файлами/Files/src/Main.java
@@ -1,30 +1,47 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+        //Старый интерфейс
+        File oldTestDir = new File(System.getProperty("java.class.path")+File.separator+"oldTestDir");
+        File oldTestTxtFile = new File(oldTestDir, "oldTestTxtFile.txt");
+
+        boolean dirIsCreated = oldTestDir.mkdir();
+        if(dirIsCreated){
+            System.out.println("Папка "+oldTestDir.getName()+" создана");
+        }
+
+        try (FileWriter fileWriter = new FileWriter(oldTestTxtFile)) {
 
 
-        //Path TestDir = Paths.get(System.getProperty("java.class.path"));
-         //testTxtFile = ;
+        }catch (IOException e){
+            System.out.println("Ошибка ввода-вывода! "+e.getMessage());
+        }
+
+        //oldTestTxtFile.
 
 
-        /*for (Path i: testFile){
-            System.out.println(i);
+
+
+
+        /*try{
+            Path testTxtFile  = Files.createFile(Paths.get(System.getProperty("java.class.path"), "testFile.txt"));
+
+        }catch (FileAlreadyExistsException e){
+
+            System.out.println("Файл "+e.getMessage()+" уже существует!");
+
+        }catch (IOException e){
+            System.out.println("Ошибка ввода-вывода! "+e.getMessage());
         }*/
-    try{
-        Path testTxtFile  = Files.createFile(Paths.get(System.getProperty("java.class.path"), "testFile.txt"));
-
-    }catch (FileAlreadyExistsException e){
-
-        System.out.println("Файл "+e.getMessage()+" уже существует!");
-
-    }catch (IOException e){
-        System.out.println("Ошибка ввода-вывода! "+e.getMessage());
-    }
 
 
 
