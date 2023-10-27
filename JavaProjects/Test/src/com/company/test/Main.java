5e939c26b890700eaa1892685b108fb6a39e7366
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-10-27 17:36:37 +0500

    27.10.23_17:36

--- JavaProjects/Test/src/com/company/test/Main.java
+++ JavaProjects/Test/src/com/company/test/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HashMap<Integer,String> hashMap = new HashMap<Integer, String>();
+        /*HashMap<Integer,String> hashMap = new HashMap<Integer, String>();
 
         System.out.println(hashMap.put(0,"Ноль"));
         System.out.println(hashMap.put(1,"Один"));
@@ -16,10 +16,34 @@ public class Main {
 
         System.out.println(hashMap.containsKey(2));
 
-        System.out.println(hashMap);
+        System.out.println(hashMap);*/
 
 
+        /*Double d1 = Double.valueOf(1/0.);
+        Double d2 = Double.valueOf(0/0.);
 
+        double d3 = 0.;
+
+        System.out.println(d1.isInfinite());
+        System.out.println(d1.isNaN());
+        System.out.println(d2.isInfinite());
+        System.out.println(d2.isNaN());
+
+        System.out.println(d3);*/
+
+        //System.out.println(Boolean.parseBoolean("TrUe"));
+
+        /*Runtime r = Runtime.getRuntime();
+        Process p = null;
+
+        try {
+            p = r.exec("notepad");
+        }catch (Exception e){
+            System.out.print("Ошибка");
+        }*/
+
+        double d = ((System.currentTimeMillis()/1000)/60/60/24) / 366 ;
+        System.out.print(d);
 
     }
 }
