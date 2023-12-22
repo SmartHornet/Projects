d16a49b26afb8a04e82814bb0b3a9f42b99d3e2e
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2023-12-22 17:08:20 +0500

    22.12.23_17:08

--- JavaProjects/Test/src/com/company/test/Main.java
+++ JavaProjects/Test/src/com/company/test/Main.java
@@ -1,49 +1,15 @@
 package com.company.test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-
 public class Main {
 
     public static void main(String[] args) {
 
-        /*HashMap<Integer,String> hashMap = new HashMap<Integer, String>();
+        System.out.println("Qwerty");
 
-        System.out.println(hashMap.put(0,"Ноль"));
-        System.out.println(hashMap.put(1,"Один"));
-        System.out.println(hashMap.replace(1,"Один-2"));
+        String str;
 
-        System.out.println(hashMap.containsKey(2));
-
-        System.out.println(hashMap);*/
-
-
-        /*Double d1 = Double.valueOf(1/0.);
-        Double d2 = Double.valueOf(0/0.);
-
-        double d3 = 0.;
-
-        System.out.println(d1.isInfinite());
-        System.out.println(d1.isNaN());
-        System.out.println(d2.isInfinite());
-        System.out.println(d2.isNaN());
-
-        System.out.println(d3);*/
-
-        //System.out.println(Boolean.parseBoolean("TrUe"));
-
-        /*Runtime r = Runtime.getRuntime();
-        Process p = null;
-
-        try {
-            p = r.exec("notepad");
-        }catch (Exception e){
-            System.out.print("Ошибка");
-        }*/
-
-        double d = ((System.currentTimeMillis()/1000)/60/60/24) / 366 ;
-        System.out.print(d);
+        str = if(1) "123454";
+        else "654321";
 
     }
 }
--- KotlinProjects/Test2/src/main/kotlin/Main.kt
+++ KotlinProjects/Test2/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+
+    println(1 in 3 downTo 1);
+
 }
