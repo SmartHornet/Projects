64357deb0199f60e6038aedf4ce7b16d3a6868b9
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2024-03-04 17:57:31 +0500

    04.03.24_17:57

--- KotlinProjects/Test2/src/main/kotlin/Main.kt
+++ KotlinProjects/Test2/src/main/kotlin/Main.kt
@@ -1,72 +1,72 @@
 package org.example
+import java.io.*
 
-class test(var name: String)
+//while(readln().equals("*")) task2()
 
-class rectangle( val a: Int, val b: Int){
-    val isSquare: Boolean
-        get(){
-            return  a == b
-        }
+fun main() {
+        //println(readln().split(Regex("\\s")).sumOf {it.toInt()})  TASK1-MINIMUM
+    var inFileStream = BufferedReader(FileReader("in.txt"))
+    var sumn = inFileStream.readLine().split(Regex("\\s")).sumOf {it.toInt()}
+    inFileStream.close()
+
+    var outFileStream = BufferedWriter(FileWriter("out.txt"))
+    outFileStream.write(sumn.toString())
+    outFileStream.close()
+
 }
 
-fun task1_fullProtect(){
-    println("Введите числа через пробел:")
-    var input = readln()
-    if(input.isNotBlank()){
-        input = input.filter {it.isDigit() || it.isWhitespace()}
-        println(input)
-        input = input.trim()
-        var strList = input.split(Regex("\\s+"))
-        println(strList)
-        var sumn = strList.sumOf {it.toInt()}
+fun task2() {
+    try{
+        var inFileStream = BufferedReader(FileReader("in.txt"))
+        var input = inFileStream.readLine();
+        inFileStream.close()
 
+        var sumn = 0
+        if(input.isNotBlank()) {
+            sumn = sumOfString(input)
+            println("Сумма: $sumn")
+        } else println("Неверный формат ввода!")
 
-        /*println("Введены следующие числа:")
-        var sum = 0;
-        for(str in strList) {
-            if (str.isNotBlank()) {
-                println(str);
-                sum += str.toInt();
-            }
-        }
-        println("Сумма1: $sum");*/
-        println("Сумма2: $sumn")
-
-    } else println("Неверный формат ввода!")
+        var outFileStream = BufferedWriter(FileWriter("out.txt"))
+        outFileStream.write(sumn.toString())
+        outFileStream.close()
+    } catch (e: Exception){
+        println("Ошибка ввода-вывода: $e")
+    }
 }
 
 fun task1(){
     println("Введите числа через пробел:")
-    var input = readln();
-    var strList = input.split(' ')
-
-
-    if(input.isNotBlank()){
-        input = input.filter {it.isDigit() || it.isWhitespace()}
-        println(input);
-        input = input.replace(Regex("\\s+"), " ")
-        println(input);
-
-
-        println(strList)
-        println("Введены следующие числа:")
-        var sum = 0;
-        for(str in strList) {
-            if (str.isNotBlank()) {
-                println(str);
-                sum += str.toInt();
-            }
-        }
-        println("Сумма: $sum");
-
+    val input = readln()
+    var sumn = 0
+    if(input.isNotBlank()) {
+        sumn = sumOfString(input)
+        println("Сумма: $sumn")
     } else println("Неверный формат ввода!")
+
 }
 
 
-fun main() {
-    while(readln().equals("*")) task1_fullProtect()
+fun sumOfString(str: String): Int =
+    str.filter {it.isDigit() || it.isWhitespace() || it == '-'}.trim().split(Regex("\\s+")).sumOf {it.toInt()}
 
-    //println('1'.isWhitespace())
+
+fun task1_sumOfString_protect() {
+
+    println("Введите числа через пробел:")
+    var input = readln()
+    if(input.isNotBlank()){
+        input = input.filter {it.isDigit() || it.isWhitespace() || it == '-'}
+        println(input)
+        input = input.trim()
+        var strList = input.split(Regex("\\s+"))
+        println(strList)
+        var sumn = 0
+        sumn = strList.sumOf {it.toInt()}
+
+        println("Сумма: $sumn")
+
+    } else println("Неверный формат строки!")
 }
 
 
@@ -90,4 +90,11 @@ fun test(index: Int, num: Int = 0)
 
 }
 
+class test(var name: String)
 
+class rectangle( val a: Int, val b: Int){
+    val isSquare: Boolean
+        get(){
+            return  a == b
+        }
+}
