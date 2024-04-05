3540b983d5a330283555629411dd1ac0569aa309
Kazmiruk Yuriy <yuriyhackit@gmail.com>
2024-04-05 18:27:34 +0500

    05.04.24_18:27

--- KotlinProjects/YandexCoderun/src/main/kotlin/Main.kt
+++ KotlinProjects/YandexCoderun/src/main/kotlin/Main.kt
@@ -1,32 +1,41 @@
 
 fun task78(){ //78. Скорая помощь https://coderun.yandex.ru/problem/ambulance
-    val k1 = 0; val k2 = 0  //квартира
-    val n1 = 0; val n2 = 0  //этаж
-    val m = 0               //количество этажей в доме
-    val p1 = 0; val p2 = 0  //номер подьезда
+    val k1 = 12; val k2 = 18  //квартира
+    var n1 = 0; val n2 = 2  //этаж
+    val m = 3               //количество этажей в доме
+    var p1 = 0; var p2 = 2  //номер подьезда
     var x = 0               //количество квартир на этаже
+
+    K1​, MM, K2K2​, P2P2​, N2N
 
     //поиск значения количества квартир на этаже методом подбора
     var kMin = 0
     var kMax = 0
+    var xIsFind = false
 
-    for(i in 2..1000000){
-        x = i
-        kMin = (p2 -1)*m*x+(n2-1)*x
-        kMax = (p2 -1)*m*x+n2*x
+    for(i in 1..1000000){
+        kMin = (p2 -1)*m*i+(n2-1)*i
+        kMax = (p2 -1)*m*i+n2*i
         if((k2 <= kMax) && (k2 > kMin)){
             x = i
-            i = 1000000
+            xIsFind = true
+            break
         }
     }
 
+    println("xIsFind: $xIsFind")
+    println("Квартир на этаж: $x")
 
+    p1 = Math.ceilDiv(k1, m*x)
+    println("Подьезд: $p1")
 
+    n1 = Math.ceilDiv(k1, x) - (p1-1)*m
+    println("Этаж: $n1")
 
 }
 
 fun main(args: Array<String>) {
 
-
+    task78()
     //println("Program arguments: ${args.joinToString()}")
 }
